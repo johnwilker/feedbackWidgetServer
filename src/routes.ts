a3105e61641a8539c20c5ad217e51f08963b8e08
John Wilker <johnwilker2@gmail.com>
2023-02-23 07:41:43 -0300

    mail test and refactor

---
+++ src/routes.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import { prisma } from './prisma';
+
+export const routes = express.Router();
+
+const transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "1714066d796961",
+    pass: "cadb54694a32cb"
+  }
+});
+
+routes.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'John Wilker <johnwilker2@gmail.com>',
+    subject: 'Novo Feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`
+    ].join('\n')
+  })
+  
+  return res.status(201).json({data: feedback})
+})
